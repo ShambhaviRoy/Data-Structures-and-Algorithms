78f2d3a941cfa8dcc5272bf0c25b1c8ed87cc9c2
Shambhavi Roy <sr3767@columbia.edu>
2021-08-29 17:04:38 -0400

    Update 1.5_One_Away.py

--- Arrays and Strings/1.5_One_Away.py
+++ Arrays and Strings/1.5_One_Away.py
@@ -2,10 +2,10 @@
 # remove a character, or replace a character. Given two strings, write a function to check if they are
 # one edit (or zero edits) away.
 # EXAMPLE
-# pale, ple -) true
-# pales, pale -) true
-# pale, bale -) true
-# pale, bae -) false
+# pale, ple -> true
+# pales, pale -> true
+# pale, bale -> true
+# pale, bae -> false
 
 
 def oneEditAway(str1, str2):
