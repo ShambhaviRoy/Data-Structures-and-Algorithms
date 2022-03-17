03c384f05931b4cda78dba31be35d6556f480fb4
shambhavi@graphen.ai <royshambhavi306@gmail.com>
2022-03-17 09:41:56 -0400

    Create contiguous_subarrays.py

---
+++ Miscellaneous-Practice-Questions/contiguous_subarrays.py
@@ -0,0 +1,40 @@
+# Find the number of contiguous subarrays in a sorted integer array
+# Example: input arr = [0, 1, 2, 4, 5, 7]
+# Output = 3 
+# The contiguous subarrays are [0, 1, 2], [4, 5], [7]
+
+
+# 2 pointer
+
+def find_cont_subarrays(arr):
+    n = len(arr)
+    i, end = 0, 0
+
+    ans = []
+    subarray = []
+
+    while i < n:
+        end = arr[i]
+        while end < n-1 and arr[end] + 1 == arr[end]:
+            subarray.append(arr[i])
+            end += 1
+        ans.append(subarray)
+        i = end + 1
+        subarray = []
+
+    return ans
+
+
+if __name__ == '__main__':
+    print('Enter length of arr:')
+    n = int(input())
+    arr = []
+
+    print('Enter elements of arr:')
+    for _ in range(n):
+        arr.append(int(input()))
+
+    ans = find_cont_subarrays(arr)
+    print(ans)
+    print(len(ans))
+    
